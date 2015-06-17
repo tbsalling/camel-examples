378e4842c7ed811fa851e672b59d6d3004b8b947
Thomas Borg Salling <tbsalling@tbsalling.dk>
2015-06-17 09:58:41 +0200

    Tokenize input string

--- 02-components-endpoints-routes/02-01-file-to-stream/src/main/java/dk/tbsalling/training/camel/app/Application.java
+++ 02-components-endpoints-routes/02-01-file-to-stream/src/main/java/dk/tbsalling/training/camel/app/Application.java
@@ -17,11 +17,24 @@ public class Application {
         }
     };
 
+    public static final RouteBuilder ROUTE_BUILDER_2 = new RouteBuilder() {
+        @Override
+        public void configure() throws Exception {
+            from("file://src/data?noop=true")
+            .split().tokenize("\n")
+            //.filter()
+            //    .simple("${body} regex '\\!..VDM,1,1,(.*)'")
+            //.end()
+            .filter(body().regex("^\\!..VDM,1,1,.*$"))
+            .to("stream:out");
+        }
+    };
+
     public static void main(String[] args) throws Exception {
         Main main = new Main();
         main.enableHangupSupport();
 
-        main.addRouteBuilder(ROUTE_BUILDER_1);
+        main.addRouteBuilder(ROUTE_BUILDER_2);
 
         main.run(args);
     }
