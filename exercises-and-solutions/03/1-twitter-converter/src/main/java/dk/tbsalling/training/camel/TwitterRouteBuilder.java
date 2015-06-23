4062f74e55ea65a4e4965d19ee2c5625467582f9
Thomas Borg Salling <tbsalling@tbsalling.dk>
2015-06-23 15:38:19 +0200

    Added exercise.

---
+++ exercises-and-solutions/03/1-twitter-converter/src/main/java/dk/tbsalling/training/camel/TwitterRouteBuilder.java
@@ -0,0 +1,24 @@
+package dk.tbsalling.training.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.interceptor.Tracer;
+
+/**
+ * A Camel Java DSL Router
+ */
+public class TwitterRouteBuilder extends org.apache.camel.builder.RouteBuilder {
+
+    final String twitterConsumerKey = System.getenv("TWITTER_CONSUMER_KEY");
+    final String twitterConsumerSecret = System.getenv("TWITTER_CONSUMER_SECRET");
+    final String twitterAccessToken = System.getenv("TWITTER_CONSUMER_ACCESS_TOKEN");
+    final String twitterAccessTokenSecret = System.getenv("TWITTER_CONSUMER_ACCESS_TOKEN_SECRET");
+
+    public void configure() {
+        getContext().addInterceptStrategy(new Tracer());
+
+        from("twitter://streaming/filter?type=event&keywords=camel&delay=10&consumerKey=" + twitterConsumerKey + "&consumerSecret=" + twitterConsumerSecret + "&accessToken=" + twitterAccessToken + "&accessTokenSecret=" + twitterAccessTokenSecret)
+        .convertBodyTo(String.class)
+        .to("stream:out")   ;
+    }
+
+}
